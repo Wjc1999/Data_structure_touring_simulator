7335235bb416b86dab1e8bc380da0a80e2ea116c
L-Jay1999 <39237952+L-Jay1999@users.noreply.github.com>
2019-03-13 19:30:45 +0800

    head修改
    id_map.h
    path.h

--- src/id_map.h
+++ src/id_map.h
@@ -1,15 +1,22 @@
 enum mode
 {
-    CITY,
-    TRAIN
+  //代表是城市
+  CITY,
+  //代表是火车
+  TRAIN
 };
 class IDMap
 {
   public:
-    // 待定
+    /*给序号获得城市名
+    */
+    string get_citystr(int city_id);
+    /*给序号获得火车座位等级名
+    */
+    string get_trainstr(int train_id);
   private:
-    istream LoadCityID(istream file, mode m);  // 加载文件
-    istream LoadTrainID(istream file, mode m); // 加载文件
-    map<city_id, city_str> city_map;
-    map<train_id, trian_str> train_map;
+    istream LoadCityID(istream file, mode m);  // 加载城市与序号的对应文件
+    istream LoadTrainID(istream file, mode m); // 加载火车等级与序号的对应文件
+    map <city_id, city_str> city_map;
+    map <train_id, trian_str> train_map;
 }；
--- src/path.h
+++ src/path.h
@@ -1,16 +1,31 @@
+#ifndef SRC_PATH
+#define SRC_PATH
+#include"time_format.h"
+#include <vector>
+using city_id = int;
+using transport_id = std::string;
+
 struct PathNode // 路径节点
 {
-    city_id city;
-    Time arrive_time;
-    transport_id transport_type;
+    city_id city;//城市名
+    int nodetype;//规定0为“起点”，1为“其它点”，
+    /*以下相当于一种交通方法的信息
+    */
+    Time depart_time;//之前离开的时间
+    Time arrive_time;//到达时间
+    int price;//来此地的花费
+    transport_id transport_type;//乘坐来的方法
 };
 class Path // 路径
 {
   public:
-    Path &path_append(city_id);
+    /*通过一个id添加至路径
+    */
+    Path &path_append(transport_id);
 
   private:
-    vector<PathNode> cities_;
-    int total_price_;
-    Time time_cost_;
+    vector <PathNode> cities_;//储存节点
+    int total_price_;//总价
+    Time total_timecost_;//总时间
 };
+#endif//SRC_PATH
