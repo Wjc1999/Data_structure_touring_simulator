b66b0eab3ed66658ec2ffd419957afd76599268d
ArCan314 <arcane314@qq.com>
2019-03-13 19:32:26 +0800

    更新部分类声明

--- src/traveller.h
+++ src/traveller.h
@@ -1,10 +1,22 @@
+#ifndef SRC_TRAVELLER
+#define SRC_TRAVELLER
+#include <vector>
+#include <iostream>
+#include "path.h"
+
+
 class Traveller // 旅行者
 {
   public:
-    // 待定
+    using Strategy = int;
+    using City_id = int;
+    Traveller();
+
   private:
-    stragety stragety;
-    vector<city_id> travelling_plan;  // 旅行计划
+    Strategy strategy;  // 旅行策略
+    std::vector<City_id> travelling_plan;  // 旅行计划
     Path touring_path;                // 旅行路径
-    vector<Path>::iterator next_city; // 路径中的下一个城市
+    std::vector<Path>::iterator next_city; // 路径中的下一个城市
 };
+
+#endif // SRC_TRAVELLER
