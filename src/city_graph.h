ad04c8130a59100788d6899749073072531148eb
ArCan314 <arcane314@qq.com>
2019-03-14 22:26:53 +0800

    update id_map.h
    修改了city_id.txt的格式
    更新id_map.h的定义
    修改Strategy的类型
    对id_map.h增加了测试

--- src/city_graph.h
+++ src/city_graph.h
@@ -1,14 +1,16 @@
 #ifndef SRC_CG
 #define SRC_CG
+
 #include <iostream>
 #include <vector>
 #include <fstream>
 #include <string>
 #include <sstream>
+
 #include "user_type.h"
 #include "time_format.h"
 #include "path.h"
-#define City_number 31
+const int City_number = 31;
 
 struct Route
 {
@@ -18,55 +20,68 @@ struct Route
   Time end_time;
   int price;
 };
+
 class CityGraph // 城市图
 {
 public:
   CityGraph();
-  Path get_path(const std::vector <City_id> &plan, Strategy s);
+  Path get_path(const std::vector<City_id> &plan, Strategy s, Time limit_time = Time(1, 0));
 
 #ifdef TEST_CG
 #include <iostream>
   //测试用
   void print()
   {
-    for(int i=0;i<5;i++)
-      for(int j=0;j<5;j++){
-        if(i!=j){
-          for(int k=0;k<city[i][j].size();k++){
-            std::cout<<i<<' '<<j<<' ';
-            std::cout<<city[i][j].at(k).transport_type<<' '<<city[i][j].at(k).price<<std::endl;
+    for (int i = 0; i < 5; i++)
+      for (int j = 0; j < 5; j++)
+      {
+        if (i != j)
+        {
+          for (int k = 0; k < city[i][j].size(); k++)
+          {
+            std::cout << i << ' ' << j << ' ';
+            std::cout << city[i][j].at(k).transport_type << ' ' << city[i][j].at(k).price << std::endl;
           }
         }
       }
   }
 #endif // TEST_CG
-  
+
 private:
-  bool LoadCityGraph(std::string name, int type);         // 加载文件
-  std::vector <Route> city[City_number][City_number];
+    bool LoadCityGraph(std::string name, int type); // 加载文件
+  std::vector<Route> city[City_number][City_number];
 };
 
-inline CityGraph::CityGraph(){
+inline CityGraph::CityGraph()
+{
   std::string flightfn = "../data/flight_extract_with_id.txt";
   std::string trainfn = "../data/train_extract_with_id.txt";
   std::string carfn = "../data/car_extract_with_id.txt";
-  if(
-  LoadCityGraph(flightfn, 2)&&
-  LoadCityGraph(trainfn, 1)&&
-  LoadCityGraph(carfn, 0))
-  std::cout<<"Loading succeed!"<<std::endl;
-  else std::cout<<"Loading error!"<<std::endl;
+  if (
+      LoadCityGraph(flightfn, 2) &&
+      LoadCityGraph(trainfn, 1) &&
+      LoadCityGraph(carfn, 0))
+    std::cout << "Loading succeed!" << std::endl;
+  else
+    std::cout << "Loading error!" << std::endl;
 }
 
-inline bool CityGraph::LoadCityGraph(std::string name, int type){
+inline bool CityGraph::LoadCityGraph(std::string name, int type)
+{
   std::ifstream stream(name);
-  if(type != 1){
-    if(stream.is_open()){
+  if (type != 1)
+  {
+    if (stream.is_open())
+    {
       int data[5];
-      while(!stream.eof()){
-        for(int i=0;i<5;i++){stream>>data[i];}
+      while (!stream.eof())
+      {
+        for (int i = 0; i < 5; i++)
+        {
+          stream >> data[i];
+        }
         //Route temp;
-        Route temp = {type,0,Time(data[2]),Time(data[3]),data[4]};
+        Route temp = {type, 0, Time(data[2]), Time(data[3]), data[4]};
         //temp.transport_type = type;
         //temp.train_seat_type = ;
         //temp.start_time = Time(data[2]);
@@ -78,14 +93,20 @@ inline bool CityGraph::LoadCityGraph(std::string name, int type){
     }
     return false;
   }
-  else{
-    if(stream.is_open()){
+  else
+  {
+    if (stream.is_open())
+    {
       int data[5];
       std::string line;
-      while(getline(stream,line)){
+      while (getline(stream, line))
+      {
         std::istringstream ss(line);
-        for(int i=0;i<7;i++){ss>>data[i];}
-        Route temp = {type,0,Time(data[2]),Time(data[3]),data[6]};
+        for (int i = 0; i < 7; i++)
+        {
+          ss >> data[i];
+        }
+        Route temp = {type, 0, Time(data[2]), Time(data[3]), data[6]};
         city[data[0]][data[1]].push_back(temp);
       }
       return true;
