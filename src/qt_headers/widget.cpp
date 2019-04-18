9a88cd526014c5197069b07804d3858dc68507e2
ArCan314 <arcane314@qq.com>
2019-04-18 23:25:39 +0800

    pathpage增加表格

--- src/qt_headers/widget.cpp
+++ src/qt_headers/widget.cpp
@@ -1,16 +1,24 @@
 ﻿#ifndef SRC_WIDGET
 #define SRC_WIDGET
 #pragma execution_character_set("utf-8")
+#include <string>
+#include <sstream>
 
 #include "widget.h"
 #include <QString>
 #include <QWidget>
 #include <QMessageBox>
+#include <QTableWidget>
+#include <QStringList>
+#include <QTableWidgetItem>
 
 Widget::Widget(QWidget *parent) : QWidget(parent),
                                   ui(new Ui::Widget)
 {
     ui->setupUi(this);
+
+    ui->Path_tableWidget->setEditTriggers(QAbstractItemView::NoEditTriggers);
+
     ui->stackedWidget->setCurrentWidget(ui->LoginPage);
 }
 
@@ -60,12 +68,12 @@ void Widget::on_SignUpButton_released() //注册
 
     if (!account_name.size())
     {
-        QMessageBox::warning(this, "Warning!", "输入为空", QMessageBox::Ok);
+        QMessageBox::warning(this, tr("Warning!"), tr("输入为空"), QMessageBox::Ok);
         return;
     }
     else if (!IsValidName(account_name.toStdString()))
     {
-        QMessageBox::warning(this, "Warning!", "非法的用户名,请重新输入", QMessageBox::Ok);
+        QMessageBox::warning(this, tr("Warning!"), "非法的用户名,请重新输入", QMessageBox::Ok);
         return;
     }
 
@@ -73,7 +81,7 @@ void Widget::on_SignUpButton_released() //注册
 
     if (account_check != -1)
     {
-        QMessageBox::warning(this, "Warning!", "该账号已被注册，请重新输入", QMessageBox::Ok);
+        QMessageBox::warning(this, tr("Warning!"), tr("该账号已被注册，请重新输入"), QMessageBox::Ok);
         return;
     }
     else
@@ -97,7 +105,7 @@ void Widget::on_StatePageButton_released() // 状态查询
 void Widget::on_QueryPathPageButton_released() // 路线查询
 {
     ui->stackedWidget->setCurrentWidget(ui->QueryPage);
-    ui->PathtextBrowser->clear();
+    ui->Path_tableWidget->clearContents();
 }
 
 void Widget::on_SimulationPageButton_released() //开始模拟
@@ -124,57 +132,63 @@ void Widget::on_QueryPathButton_released()
 {
     City_id start_city = ui->origin_comboBox->currentIndex();
     City_id target_city = ui->target_comboBox->currentIndex();
+    ui->Path_tableWidget->clearContents();
+
     if (start_city == target_city)
     {
-        QMessageBox::warning(this, "Warning!", "始发地不能与目的地一样", QMessageBox::Ok);
+        QMessageBox::warning(this, tr("Warning!"), tr("始发地不能与目的地一样"), QMessageBox::Ok);
         return;
     }
 
     int size = citygraph_widget.Getsize(start_city, target_city);
     if (!size)
     {
-        QMessageBox::warning(this, "Warning!", "两城市间无路线", QMessageBox::Ok);
+        QMessageBox::warning(this, tr("Warning!"), tr("两城市间无路线"), QMessageBox::Ok);
     }
     else
     {
-        ui->PathtextBrowser->clear();
-        QString qtemp;
-        std::string comp("三个字");
-        std::string wrap[] = {"\t\t", "\t"};
-        qtemp = "序号\t始发地\t\t目的地\t\t方式\t出发时间\t到达时间\t价格\t";
-        /*temp += "序号" += "\t"
-             += "始发地"
-             += "\t\t"
-             += "目的地"
-             += "\t\t"
-             += "方式" += "\t"
-             += "出发时间" += "\t"
-             += "到达时间" += "\t"
-             += "价格" += "\t";*/
-        ui->PathtextBrowser->append(qtemp);
-        std::string temp;
-        for (int k = 0; k < size; k++)
-        {
-            qtemp.clear();
-            temp.clear();
-            Route route = citygraph_widget.GetRoute(start_city, target_city, k);
+        auto &path_table = ui->Path_tableWidget;
+        path_table->setRowCount(size);
 
-            std::string cityi = idmap_widget.GetCityStr(start_city);
-            std::string cityj = idmap_widget.GetCityStr(target_city);
-            std::string method = idmap_widget.GetTransStr(route.transport_type);
-            temp += std::to_string(k + 1) + "\t";
-            temp += cityi + wrap[cityi.size() > comp.size()];
-            temp += cityj + wrap[cityj.size() > comp.size()];
-            temp += method + "\t";
-            temp += RouteShow(route.start_time, route.end_time);
-            temp += std::to_string(route.price) + "\t";
-            /*std::cout << k + 1 << '\t'
-                      << cityi << wrap[cityi.size() > comp.size()]
-                      << cityj << wrap[cityj.size() > comp.size()]
-                      << method << '\t';
-            RouteShow(route.start_time, route.end_time);
-            std::cout << route.price << '\t' << std::endl;*/
-            ui->PathtextBrowser->append(qtemp.fromStdString(temp));
+        QString start_city_qstr, target_city_qstr, transport_type_qstr, start_time_qstr, end_time_qstr, price_qstr;
+        std::string start_time_str, end_time_str;
+        std::stringstream ss;
+
+        start_city_qstr = QString::fromStdString(idmap_widget.GetCityStr(start_city));
+        target_city_qstr = QString::fromStdString(idmap_widget.GetCityStr(target_city));
+
+        std::string temp;
+        for (int i = 0; i < size; i++)
+        {
+            Route r = citygraph_widget.GetRoute(start_city, target_city, i);
+
+            transport_type_qstr = QString::fromStdString(idmap_widget.GetTransStr(r.transport_type));
+
+            ss.str(RouteShow(r.start_time, r.end_time));
+            ss.clear();
+            ss >> start_time_str >> end_time_str;
+            start_time_qstr = QString::fromStdString(start_time_str);
+            end_time_qstr = QString::fromStdString(end_time_str);
+
+            price_qstr = QString::fromStdString(std::to_string(r.price));
+            qDebug() << start_city_qstr << " " << target_city_qstr << " " <<
+                        transport_type_qstr << " " << start_time_qstr << " " <<
+                        end_time_qstr << " " << price_qstr << endl;
+            QTableWidgetItem *id_cell = new QTableWidgetItem(tr(std::to_string(i + 1).c_str()));
+            QTableWidgetItem *start_city_cell = new QTableWidgetItem(start_city_qstr);
+            QTableWidgetItem *target_city_cell = new QTableWidgetItem(target_city_qstr);
+            QTableWidgetItem *transport_type_cell = new QTableWidgetItem(transport_type_qstr);
+            QTableWidgetItem *start_time_cell = new QTableWidgetItem(start_time_qstr);
+            QTableWidgetItem *end_time_cell = new QTableWidgetItem(end_time_qstr);
+            QTableWidgetItem *price_cell = new QTableWidgetItem(price_qstr);
+
+            path_table->setItem(i, 0, id_cell);
+            path_table->setItem(i, 1, start_city_cell);
+            path_table->setItem(i, 2, target_city_cell);
+            path_table->setItem(i, 3, transport_type_cell);
+            path_table->setItem(i, 4, start_time_cell);
+            path_table->setItem(i, 5, end_time_cell);
+            path_table->setItem(i, 6, price_cell);
         }
     }
 }
