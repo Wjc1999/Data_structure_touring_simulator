271925319fc622105f5014100548774e64714232
L-Jay1999 <39237952+L-Jay1999@users.noreply.github.com>
2019-05-28 23:07:36 +0800

    simulate update

--- src/qt_headers/simulator.cpp
+++ src/qt_headers/simulator.cpp
@@ -6,10 +6,12 @@ Simulator::Simulator(QObject *parent) : QObject(parent)
 {
 }
 
-void Simulator::initialize(QLCDNumber *lcd1, QLCDNumber *lcd2, MyMap *map, CityGraph *cg, Traveller *t)
+void Simulator::initialize(QLCDNumber *lcd1, QLCDNumber *lcd2, MyMap *map, QLabel *label, IDMap *idmap, CityGraph *cg, Traveller *t)
 {
     day_LCD_ = lcd1;
     hour_LCD_ = lcd2;
+    display_label_ = label;
+    idmap_ = idmap;
     map_ = map;
     map_->initialize(cg, t);
     set_flush_rate(1, 10); //这里可以改速率
@@ -25,6 +27,7 @@ void Simulator::ready_for_simulate()
     hour_LCD_->display(display_hour_);
     //qDebug() << simulate_onehour_s_ << display_interval_ms_ << flush_per_hour_;
     map_->ready_for_simulate();
+    display_label_->setText(QString::fromStdString(idmap_->GetCityStr(map_->traveller_path_.GetNode(0).former_city)));
 }
 
 void Simulator::set_flush_rate(int simulate_onehour_s_, int display_interval_ms_)
@@ -63,8 +66,9 @@ void Simulator::onTimerOut()
 
                 if (current_pathnode_ == map_->traveller_path_.GetLen())
                 {
+                    display_label_->setText(QString::fromStdString(idmap_->GetCityStr(map_->traveller_path_.GetNode(current_pathnode_-1).current_city)));
                     timer_.stop();
-                    is_finished = true;
+                    is_finished_ = true;
                     return;
                 }
                 else
@@ -73,12 +77,15 @@ void Simulator::onTimerOut()
                 hour_left_ = map_->traveller_->get_stay_hours(*(map_->citygraph_), current_pathnode_);
                 if (hour_left_ == 0)
                 {
+                    display_label_->setText(QString::fromStdString(idmap_->GetCityStr(map_->traveller_path_.GetNode(current_pathnode_).former_city) + " -> " + idmap_->GetCityStr(map_->traveller_path_.GetNode(current_pathnode_).current_city)));
                     hour_left_ = map_->traveller_->get_off_hours(*(map_->citygraph_), current_pathnode_);
                     current_states_ = OFF;
                 }
                 else
+                {
+                    display_label_->setText(QString::fromStdString(idmap_->GetCityStr(map_->traveller_path_.GetNode(current_pathnode_).former_city)));
                     current_states_ = STAY;
-
+                }
             }
         }
     }
@@ -86,45 +93,47 @@ void Simulator::onTimerOut()
 
 void Simulator::start()
 {
-    if (!is_start_ && !is_finished)
+    if (!is_start_ && !is_finished_)
     {
         hour_left_ = map_->traveller_->get_stay_hours(*(map_->citygraph_), 0);
         if (hour_left_ == 0)
         {
+            display_label_->setText(QString::fromStdString(idmap_->GetCityStr(map_->traveller_path_.GetNode(current_pathnode_).former_city) + " -> " + idmap_->GetCityStr(map_->traveller_path_.GetNode(current_pathnode_).current_city)));
             current_states_ = OFF;
             hour_left_ = map_->traveller_->get_off_hours(*(map_->citygraph_), 0);
         }
         else
             current_states_ = STAY;
         is_start_ = true;
-        is_finished = false;
+        is_finished_ = false;
         timer_.start();
     }
 }
 
 void Simulator::stop()
 {
-    if (is_start_ && !is_finished)
+    if (is_start_ && !is_finished_)
         timer_.stop();
 }
 
 void Simulator::continuing()
 {
-    if (is_start_ && !is_finished)
+    if (is_start_ && !is_finished_)
         timer_.start();
 }
 
 void Simulator::reset()
 {
-    if (is_start_ || is_finished)
+    if (is_start_ || is_finished_)
     {
         timer_.stop();
         display_day_ = map_->traveller_->get_init_time().GetDay() + 1;
         display_hour_ = map_->traveller_->get_init_time().GetHour();
         day_LCD_->display(display_day_);
         hour_LCD_->display(display_hour_);
+        display_label_->setText(QString::fromStdString(idmap_->GetCityStr(map_->traveller_path_.GetNode(0).former_city)));
         is_start_ = false;
-        is_finished = false;
+        is_finished_ = false;
         flush_count_ = 0;
         current_pathnode_ = 0;
         map_->reset();
